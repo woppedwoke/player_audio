a95c25d60a62457e4789e5c2fdb9a5f4ec5bd21c
woppedwoke <84482264+woppedwoke@users.noreply.github.com>
2022-03-25 16:39:05 -0300

    Add files via upload

--- js/music-list.js
+++ js/music-list.js
@@ -46,6 +46,12 @@ let allMusic = [
     img: "music-6",
     src: "music-6"
   },
+  {
+    name: "Glass Animals - Heat Waves",
+    artist: "Glass Animals",
+    img: "music-7",
+    src: "music-7"
+  },
   // like this paste it and remember to give comma after ending of this bracket }
   // {
   //   name: "Here is the music name",
