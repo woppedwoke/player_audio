8036b08adc071f0170827c381819fcce6eae8460
woppedwoke <84482264+woppedwoke@users.noreply.github.com>
2022-03-27 22:31:49 -0300

    Add files via upload

--- js/music-list.js
+++ js/music-list.js
@@ -58,6 +58,12 @@ let allMusic = [
     img: "music-8",
     src: "music-8"
   },
+  {
+    name: "Океан Ельзи  Обними (Callmearco Remix)",
+    artist: "Callmearco",
+    img: "music-9",
+    src: "music-9"
+  },
   // like this paste it and remember to give comma after ending of this bracket }
   // {
   //   name: "Here is the music name",
